c2e437ecc9b33df32e914eae169ea5f402808d13
AlinuerA <alinvrablikim@gmail.com>
2022-01-24 07:56:36 -0800

    Topic 1

--- src/HelloGit/TestCase01.java
+++ src/HelloGit/TestCase01.java
@@ -3,5 +3,6 @@ package HelloGit;
 public class TestCase01 {
     public static void main(String[] args) {
         System.out.println("Hello Monday");
+        System.out.println("Today 's Topic is Creating new branch ");
     }
 }
